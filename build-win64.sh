4b9342e16895c25a922c493ced2975942cae273a
ThanosApostolou <thanosapostolou@outlook.com>
2017-09-26 23:29:48 +0300

    fix library name, add full win64 build support

---
+++ build-win64.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+mkdir -p ./tapocalc-win64
+meson --cross-file build-win64.config --buildtype=release builddir-win64
+cd builddir-win64
+ninja
+cp *.exe ../tapocalc-win64
+cp *.dll ../tapocalc-win64
+cd ../tapocalc-win64
+rm -r ../builddir-win64
+cp /usr/x86_64-w64-mingw32/bin/libgcc_s_seh-1.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libwinpthread-1.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libstdc++-6.dll ./
